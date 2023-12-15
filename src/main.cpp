4da58ce749f2d1961ce777b6e35cae28c64def0c
auziqni <65333301+auziqni@users.noreply.github.com.>
2023-12-15 09:55:54 +0700

    init device blink

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+void setup()
+{
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+  Serial.println("hidup");
+  delay(50);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
+  Serial.println("mati");
+  delay(50); // wait for a second
+}
