132993282a49302659e0f0f4efbb1366b29a188b
auziqni <65333301+auziqni@users.noreply.github.com.>
2023-12-15 11:47:10 +0700

    comp: soil humid sens

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,26 @@
 #include <Arduino.h>
-#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
 
-static const int RXPin = 36, TXPin = 39;
-static const uint32_t GPSBaud = 9600;
+int soil_pin = 15; // 2
 
-TinyGPSPlus gps;
-
-SoftwareSerial ss(RXPin, TXPin);
-
+int val_soil;
 void setup()
 {
   Serial.begin(115200);
-  ss.begin(GPSBaud);
+  pinMode(soil_pin, INPUT);
 }
 
 void loop()
 {
-  if (millis() > 1000 && gps.charsProcessed() < 10)
-    Serial.println(F("No GPS data received: check wiring"));
+  float raw_val_soil = analogRead(soil_pin);
 
-  while (ss.available() > 0)
-  {
-    byte gpsData = ss.read();
-    // Serial.write(gpsData);
+  val_soil = ((4095 - raw_val_soil) / 4095) * 100;
 
-    gps.encode(gpsData);
-    if (gps.location.isValid())
-    {
-      Serial.print("Latitude= ");
-      Serial.print(gps.location.lat(), 6);
-      Serial.print(" Longitude= ");
-      Serial.println(gps.location.lng(), 6);
-    }
-    else
-    {
-      Serial.print("waiting gps");
-    }
-  }
+  Serial.print("soil raw= ");
+  Serial.print(raw_val_soil);
+  Serial.print(", soil= ");
+  Serial.println(val_soil);
+
+  Serial.println("");
+
+  delay(1000);
 }
